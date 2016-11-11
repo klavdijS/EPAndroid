9ebb9f696e126a5708211e8e277ffbb90a4ee68e
kmetonzl <npovsic@gmail.com>
2016-11-11 18:12:51 +0100

    Initial commit

---
+++ app/src/main/java/sport_thinking/viewevents/activities/Home.java
@@ -0,0 +1,15 @@
+package sport_thinking.viewevents.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sport_thinking.viewevents.R;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
